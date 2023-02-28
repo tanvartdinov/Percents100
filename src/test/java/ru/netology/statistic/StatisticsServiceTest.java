961bf85772e1a04909f4efa9fa218ccbd872081c
Timur Anvartdinov <t.anvartdinov@life-stream.tv>
2023-02-28 20:25:51 +0300

    add extra test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,11 +6,23 @@ import org.junit.jupiter.api.Test;
 public class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMaxWhenMaximumInBegin() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWhenMaximumInMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 50, 3, 8, 6, 11, 11, 12};
+        long expected = 50;
 
         long actual = service.findMax(incomesInBillions);
 
